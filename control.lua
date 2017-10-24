5cb015c135b2660aaaafd90cac7796e43ecc8e24
BeefEX <janpolak.lysa@gmail.com>
2017-10-24 13:15:39 +0200

    Introduced Owner categories, bumped version to 1.0.0

--- control.lua
+++ control.lua
@@ -1,8 +1,8 @@
-local default_owner_interface_name = "default_surface_owner"
-
--- Default surface owner
+-- Default general surface owner
 remote.add_interface( default_owner_interface_name, {
-    
+    get_owner_mod = function ( )
+        return "unknown"
+    end
 } )
 
 -- Initializes all global variables of this mod.
@@ -13,6 +13,8 @@ local function init(  )
 
     -- Name of the surface that is currently getting created by the library, needed to ignore the creation event of it.
     global.currently_processing_surface = ""
+
+    global.default_owner_interface = {}
 end
 
 -- Interface housing all functions of this library.
@@ -29,7 +31,7 @@ remote.add_interface( "surface_owner", {
         return surface
     end,
 
-    retrieve_owner = function ( surface )
+    retrieve_owner = function ( surface, category )
         local surface_index = 0
 
         if type(surface) == "number" then
@@ -40,10 +42,14 @@ remote.add_interface( "surface_owner", {
             return nil
         end
 
-        return global.surface_owners[surface_index]
+        if not global.surface_owners[surface_index] then
+            return nil
+        end
+
+        return global.surface_owners[surface_index][category]
     end,
 
-    reassign_owner = function ( surface, owner_interface_name )
+    get_list_of_owners = function ( surface )
         local surface_index = 0
         
         if type(surface) == "number" then
@@ -54,14 +60,46 @@ remote.add_interface( "surface_owner", {
             return nil
         end
 
-        global.surface_owners[surface_index] = owner_interface_name
-    end
+        if not global.surface_owners[surface_index] then
+            return nil
+        end
+
+        local keys = {}
+
+        for key,_ in pairs(global.surface_owners) do
+            table.insert( keys, key )
+        end
+
+        return keys
+    end,
+
+    assign_owner = function ( surface, owner_category, owner_interface_name )
+        local surface_index = 0
+        
+        if type(surface) == "number" then
+            surface_index = surface
+        elseif type(surface) == "string" then
+            surface_index = game.surfaces[surface].index
+        else
+            return nil
+        end
+
+        if not global.surface_owners[surface_index] then
+            global.surface_owners[surface_index] = {}
+        end
+
+        global.surface_owners[surface_index][owner_category] = owner_interface_name
+    end,
+
+    set_default_owner = function ( owner_category, owner_interface_name )
+        global.default_owner_interface[owner_category] = owner_interface_name
+    end,
 } )
 
 -- Used to register the default owner for surfaces creating using the standard way (`game.create_surface`).
 local function on_surface_created( event )
     if not game.surfaces[event.surface_index].name == global.currently_processing_surface then
-        global.surface_owners[event.surface_index] = default_owner_interface_name
+        global.surface_owners[event.surface_index] = global.default_owner_interface
     end
 end
 
