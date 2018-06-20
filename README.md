965d9492085a5b2d5ea57d1cd7d496c019854cc2
Jan Polák <janpolak.lysa@gmail.com>
2018-06-20 11:24:02 +0200

    Migrated to a new terminology

--- README.md
+++ README.md
@@ -1,42 +1,43 @@
-# SurfaceOwnerLib
+# SurfaceControllerLib
 
 Library which allows mods to share data about surfaces using interfaces.
 
 ## Concepts
 
-This library exposes all of it's functioality in one interface, called `surface_owners`. This interface is used to register other interfaces as surface owners of a certain category.
+This library exposes all of it's functioality in one interface, called `surface_controllers`. This interface is used to register other interfaces as surface controllers of a certain category.
 
-Surface owner - An interface providing data about one or more surfaces but only about one category.
+Surface controller - An interface providing data about one or more surfaces but only about one category.
 
-Owner category - String under which an surface owner is registered, the string should be short and explainatory.
+Controller category - String under which an surface controller is registered, the string should be short and explainatory.
 
 ## Interface outline
 
- - `retrieve_owner`
-    - Returns name of the interface of the given category for the given surface.
-    - Parameters
-        - `surface` [number | string] Surface to be retrieved from.
-        - `category` [string] Category ot retrieve.
- - `list_owner_categories`
-    - Returns list of owner categories that are registered on the given surface.
-    - Parameters
-        - `surface` [number | string] Surface to be retrieved from.
- - `assign_owner`
-    - Assigns the owner of given surface for given category to the interface given.
-    - Parameters
-        - `surface` [number | string] Surface to be retrieved from.
-        - `owner_category` [string] Category to be registered.
-        - `owner_interface_name` [string] Name of the interface to be registered.
- - `set_default_owner`
-    - Assigns the default owner for given category.
-    - Parameters
-        - `owner_category` [string] Category to be registered.
-        - `owner_interface_name` [string] Name of the interface to be registered.
+- `retrieve_controller`
+  - Returns name of the interface of the given category for the given surface.
+  - Parameters
+    - `surface` [number | string] Surface to be retrieved from.
+    - `category` [string] Category ot retrieve.
+- `list_controller_categories`
+  - Returns list of controller categories that are registered on the given surface.
+  - Parameters
+    - `surface` [number | string] Surface to be retrieved from.
+- `assign_controller`
+  - Assigns the controller of given surface for given category to the interface given.
+  - Parameters
+    - `surface` [number | string] Surface to be retrieved from.
+    - `controller_category` [string] Category to be registered.
+    - `controller_interface_name` [string] Name of the interface to be registered.
+- `set_default_controller`
+  - Assigns the default controller for given category.
+  - Parameters
+    - `controller_category` [string] Category to be registered.
+    - `controller_interface_name` [string] Name of the interface to be registered.
 
-## General owner category
+## General controller category
 
-Every surface must have an owner for an "general" category which contains some basic information about the surface. The default one assigned when the surface is created.
+Every surface must have an controller for an "general" category which contains some basic information about the surface. The default one assigned when the surface is created will provide values indicating an error.
 
 Functions that should be provided by "general" interface:
- - `get_owner_mod`
-    - Returns name of the mod owning the surface.
+
+- `get_controller_mod`
+  - Returns name of the mod owning the surface.
--- control.lua
+++ control.lua
@@ -1,108 +1,114 @@
--- Default general surface owner
-remote.add_interface( "default_general_surface_interface", {
-    get_owner_mod = function ( )
-        return "unknown"
-    end
-} )
+-- Default general surface controller
+remote.add_interface(
+	"default_general_surface_interface",
+	{
+		get_controller_mod = function()
+			return "unknown"
+		end
+	}
+)
 
 -- Initializes all global variables of this mod.
-local function init(  )
-    
-    -- Table mapping surface index to interface name of the mod owning it.
-    global.surface_owners = {}
+local function init()
+	-- Table mapping surface index to interface name of the mod owning it.
+	global.surface_controllers = {}
 
-    -- Name of the surface that is currently getting created by the library, needed to ignore the creation event of it.
-    global.currently_processing_surface = ""
+	-- Name of the surface that is currently getting created by the library, needed to ignore the creation event of it.
+	global.currently_processing_surface = ""
 
-    -- Default owners
-    global.default_owner_interface = {
-        general = "default_general_surface_interface"
-    }
+	-- Default controllers
+	global.default_controller_interface = {
+		general = "default_general_surface_interface"
+	}
 end
 
 -- Interface housing all functions of this library.
-remote.add_interface( "surface_owner", {
+remote.add_interface(
+	"surface_controller",
+	{
+		--
+		-- Retrieves controller of a surface for a given category.
+		retrieve_controller = function(surface, category)
+			local surface_index = 0
 
-    -- Retrieves owner of a surface for a given category.
-    retrieve_owner = function ( surface, category )
-        local surface_index = 0
+			if type(surface) == "number" then
+				surface_index = surface
+			elseif type(surface) == "string" then
+				surface_index = game.surfaces[surface].index
+			else
+				return nil
+			end
 
-        if type(surface) == "number" then
-            surface_index = surface
-        elseif type(surface) == "string" then
-            surface_index = game.surfaces[surface].index
-        else
-            return nil
-        end
+			if not global.surface_controllers[surface_index] then
+				return nil
+			end
 
-        if not global.surface_owners[surface_index] then
-            return nil
-        end
+			return global.surface_controllers[surface_index][category]
+		end,
+		--
+		-- Retrieves list of controller categories the given surface contains.
+		list_controller_categories = function(surface)
+			local surface_index = 0
 
-        return global.surface_owners[surface_index][category]
-    end,
+			if type(surface) == "number" then
+				surface_index = surface
+			elseif type(surface) == "string" then
+				if game.surfaces[surface] == nil then
+					return nil
+				end
+				surface_index = game.surfaces[surface].index
+			else
+				return nil
+			end
 
-    -- Retrieves list of owner categories given surface contains.
-    list_owner_categories = function ( surface )
-        local surface_index = 0
-        
-        if type(surface) == "number" then
-            surface_index = surface
-        elseif type(surface) == "string" then
-            surface_index = game.surfaces[surface].index
-        else
-            return nil
-        end
+			if not global.surface_controllers[surface_index] then
+				return nil
+			end
 
-        if not global.surface_owners[surface_index] then
-            return nil
-        end
+			local keys = {}
 
-        local keys = {}
+			for key, _ in pairs(global.surface_controllers) do
+				table.insert(keys, key)
+			end
 
-        for key,_ in pairs(global.surface_owners) do
-            table.insert( keys, key )
-        end
+			return keys
+		end,
+		-- Used to assing controller to a category of a given surface.
+		assign_controller = function(surface, controller_category, controller_interface_name)
+			local surface_index = 0
 
-        return keys
-    end,
+			if type(surface) == "number" then
+				surface_index = surface
+			elseif type(surface) == "string" then
+				surface_index = game.surfaces[surface].index
+			else
+				return nil
+			end
 
-    -- Used to assing owner to a category of a given surface.
-    assign_owner = function ( surface, owner_category, owner_interface_name )
-        local surface_index = 0
-        
-        if type(surface) == "number" then
-            surface_index = surface
-        elseif type(surface) == "string" then
-            surface_index = game.surfaces[surface].index
-        else
-            return nil
-        end
+			if not global.surface_controllers[surface_index] then
+				global.surface_controllers[surface_index] = {}
+			end
 
-        if not global.surface_owners[surface_index] then
-            global.surface_owners[surface_index] = {}
-        end
+			global.surface_controllers[surface_index][controller_category] = controller_interface_name
+		end,
+		-- Used to set default controller for given category.
+		set_default_controller = function(controller_category, controller_interface_name)
+			global.default_controller_interface[controller_category] = controller_interface_name
+		end
+	}
+)
 
-        global.surface_owners[surface_index][owner_category] = owner_interface_name
-    end,
-
-    -- Used to set default owner for given category.
-    set_default_owner = function ( owner_category, owner_interface_name )
-        global.default_owner_interface[owner_category] = owner_interface_name
-    end,
-} )
-
--- Used to register the default owner for surfaces creating using the standard way (`game.create_surface`).
-local function on_surface_created( event )
-    if not game.surfaces[event.surface_index].name == global.currently_processing_surface then
-        global.surface_owners[event.surface_index] = global.default_owner_interface
-    end
+-- Used to register the default controller for surfaces creating using the standard way (`game.create_surface`).
+local function on_surface_created(event)
+	if not game.surfaces[event.surface_index].name == global.currently_processing_surface then
+		global.surface_controllers[event.surface_index] = global.default_controller_interface
+	end
 end
 
 -- Used to clean up after an surface is deleted.
-local function on_surface_deleted( event )
-    global.surface_owners[event.surface_index] = nil
+local function on_surface_deleted(event)
+	global.surface_controllers[event.surface_index] = nil
 end
 
-script.on_event( defines.events.on_surface_created, on_surface_created )
-script.on_event( defines.events.on_surface_deleted, on_surface_deleted )
+script.on_event(defines.events.on_surface_created, on_surface_created)
+script.on_event(defines.events.on_surface_deleted, on_surface_deleted)
